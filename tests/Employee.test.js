48d86608970ef9bf13322b87db84387d2f16375c
Skyler Ferreira <sky_3489@icloud.com>
2022-07-28 17:17:15 -0700

    workig test for employee lib

---
+++ tests/Employee.test.js
@@ -0,0 +1,31 @@
+const employee = require('../lib/employee')
+
+
+const michle = new employee('michle','196','michles@email.com')
+
+describe('test to see if name brakes employee', ()=>{
+const {name, ID, email} = michle
+
+it('check name', () => {expect (name).toBe('michle')})
+
+it('check ID', () =>{expect(name.toBe('196'))}) 
+
+it('check Email', () => {expect(email.toBe('michles@email.com'))})
+})
+
+describe('testing the methods for employee', () => {
+    it('check name', () =>{
+        const michleName = michle.getName();
+        expect(michleName).toBe('michle')
+    })
+
+    it('check ID', ( => {
+        const michleId = michle.getId();
+        expect(michleId).toBe('196')
+    }))
+    
+    it('check email', ()=>{
+        const michleEmail = michle.getEmail();
+        expect(michleEmail).toBe('michles@email.com')
+    })
+})
